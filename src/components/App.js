45571dab9dede3abb124a41f973c392950d0359e
sonimanish180 <68246299+sonimanish180@users.noreply.github.com>
2020-12-22 13:09:18 +0530

    changes2

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,15 @@
-import React, { Component, useState, useEffect } from "react";
+import React, { Component, useState, useEffect, useRef } from "react";
 import "../styles/App.css";
 
 const App = () => {
   // write your code here
   const [remainingTime, setRemainingTime] = React.useState("");
+  const intervalRef = useRef();
+
   const handleKeyDown = (event) => {
     if (event.keyCode === 13) {
       console.log(event.target.value + " <--");
-      setRemainingTime(event.target.value);
+      setRemainingTime(Math.floor(event.target.value));
       // decByOne();
       // useEffect(() => {
       //   setInterval(decByOne, 1000);
@@ -15,29 +17,27 @@ const App = () => {
       // startTimer();
     }
   };
-
-  let id;
+  // const setChange = () => {
+  //   changed = false;
+  // };
 
   useEffect(() => {
     console.log(remainingTime);
-    if (remainingTime === 0) {
+    if (remainingTime === 0 || !remainingTime) {
       return;
     }
-    if (remainingTime) {
-      clearTimeout(startTimer);
-      startTimer();
-    }
-
+    const timerId = setInterval(decByOne, 1000);
+    intervalRef.current = timerId;
     console.log("entered");
+    return () => {
+      clearInterval(intervalRef.current);
+    };
   });
-  const startTimer = () => {
-    id = setTimeout(decByOne, 1000);
-  };
-
   let decByOne = () => {
     // let temp = remainingTime - 1;
     // console.log(remainingTime);
     setRemainingTime(remainingTime - 1);
+    // setCount(1);
   };
 
   // const startTimer = () => setInterval(decByOne, 1000);
